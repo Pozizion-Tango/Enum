4f804feb80f703aeb6c159acddea5cbd91dff556
David F.N <99806140+Pozizion-Tango@users.noreply.github.com>
2023-02-08 09:13:08 +0100

    Add project files.

---
+++ wat/Hat.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enum
+{
+    internal class Hat
+    {
+        public enum College {Gryffendor=1,Hufflepuff,Ravenclaw,Slytherin};//enum full of colleges
+
+        public College Sort()//Easy method to choose which college
+        {
+            Random rnd = new Random();
+            return (College)rnd.Next(1, 5);//returns random college for the user.
+        }
+    }
+}
---
+++ wat/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Enum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool stop = false;//A bool used to help stop the loop when the user wants it.
+            Hat hat = new Hat();//Creates new "sorting hat"
+            while (!stop)
+            {
+                Console.WriteLine(hat.Sort());//displays what college is choosen
+                if (Console.ReadLine() == "stop")//Reads if the player wants to stop or not.
+                    stop = true;
+            }
+        }
+    }
+}
